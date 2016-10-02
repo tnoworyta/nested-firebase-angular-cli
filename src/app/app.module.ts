1d73083017dd344832a28d81b88171108ed9a68c
tnoworyta <tomasz.noworyta@gmail.com>
2016-10-02 15:34:05 +0200

    Firebase and angularfire2 installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBulVYHp3MWlg_Mx8OqmKmtQwu7Ao51ZZg",
+  authDomain: "mdoc1.firebaseapp.com",
+  databaseURL: "https://mdoc1.firebaseio.com",
+  storageBucket: "project-7103516944652309740.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
